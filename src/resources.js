d510c2b80793fe8aea2ef6011505b41bb81a52bd
Noano01 <Noano01.Noano01@gmail.com>
2022-11-06 22:24:25 +0100

    Fix resource handler so it actually works

--- src/resources.js
+++ src/resources.js
@@ -8,13 +8,21 @@
  * @param {function(string):void} callback The function to call once the resource has loaded.
  */
 export function loadResource(path, callback) {
-    var element = document.createElement("script");
-    element.type = "text/plain";
-    element.src = "resources/"+path;
-    element.onload = function() {
-        console.log("[ResourceLoader] Finished loading file \""+path+"\"");
-        callback(console.text);
-        element.remove();
+    //Create a request to load the file
+    var request = new XMLHttpRequest();
+    request.open("GET", "resources/" + path);
+    request.onreadystatechange = function () {
+        //Check if request has been handled
+        if (request.readyState == XMLHttpRequest.DONE) {
+            //Check if file was successfully retrieved
+            if (request.status == 200) {
+                console.log("[ResourceLoader] Finished loading file \"" + path + "\"");
+                callback(request.responseText);
+            } else {
+                console.log("[ResourceLoader] Failed to load file \"" + path + "\" with code " + request.status);
+            }
+            
+        }
     };
-    document.head.appendChild(element);
+    request.send();
 }
