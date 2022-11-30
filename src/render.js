b75bfae9795a3c97c99908c13fd04cfe4d60bb0c
Noano01 <Noano01.Noano01@gmail.com>
2022-11-30 10:35:47 +0100

    Prepare for renderer

--- src/render.js
+++ src/render.js
@@ -2,16 +2,29 @@
     Renders things to screen.
 */
 
+//The canvas where everything is rendered.
 const canvas = document.getElementById("canvas");
 const gl = canvas.getContext("webgl");
 
-const click_canvas = null;
-const click_gl = null;// click_canvas.getContext("webgl");
+//The canvas that is used to detect what was clicked.
+const click_canvas = document.getElementById("click_canvas");
+const click_gl = click_canvas.getContext("webgl");
 
 /**
  * Clear everything from the screen.
  */
 export function clear();
+
+/**
+ * Make sure the resolution of the canvases matches their actual size.
+ */
+export function resizeScreen() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    click_canvas.width = canvas.clientWidth;
+    click_canvas.height = canvas.clientHeight;
+}
 
 /**
  * Draw object to context
