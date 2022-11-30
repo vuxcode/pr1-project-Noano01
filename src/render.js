4c643b7f19bcf539b534e0048eeb4014e647b6c1
Noano01 <Noano01.Noano01@gmail.com>
2022-11-30 11:50:22 +0100

    Add some WebGL stuff

--- src/render.js
+++ src/render.js
@@ -13,7 +13,60 @@ const click_gl = click_canvas.getContext("webgl");
 /**
  * Clear everything from the screen.
  */
-export function clear();
+export function clear() {
+    gl.clearColor(0,0,0,0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+/**
+ * Compiles a shader from source
+ * @param {WebGLRenderingContext} context The GL context to use.
+ * @param {*} type What kind of shader it is.
+ * @param {string} source The name of the script containing the shader code.
+ * @returns The shader
+ */
+function compileShader(context, type, source) {
+    var shader = context.createShader(type);
+    //Load and compile the shader
+    context.shaderSource(shader, document.getElementById(source).text);
+    context.compileShader(shader);
+    //Check whether it compiled correctly. And if so, return it.
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return shader;
+    }
+    //Provide log and delete shader, in case of error.
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+/**
+ * Links together several shaders into a "program"
+ * @param {WebGLRenderingContext} context The GL context to use.
+ * @param {*} vertex The vertex shader.
+ * @param {*} fragment The fragment shader.
+ * @returns The shader program
+ */
+function createProgram(context, vertex, fragment) {
+    var program = context.createProgram();
+    //Attach the shaders to the program.
+    context.attachShader(program, vertex);
+    context.attachShader(program, fragment);
+    context.linkProgram(program);
+    //Check if successful.
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      return program;
+    }
+    //Provide log and delete program, in case of error.
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+export function setupShaders() {
+    var v_shader = compileShader(gl, gl.VERTEX_SHADER, "vertex_shader");
+    var f_shader = compileShader(gl, gl.FRAGMENT_SHADER, "fragment_shader");
+    var program = createProgram(gl, v_shader, f_shader);
+    
+}
 
 /**
  * Make sure the resolution of the canvases matches their actual size.
